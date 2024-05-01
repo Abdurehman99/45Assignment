d8e577b839735eb3cd9f5159dbe8117c47bcb23e
Abdurehman99 <159618780+Abdurehman99@users.noreply.github.com>
2024-05-01 06:39:59 +0500

    3.Name Cases

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-"use strict";
-let Person_Name = "Eric";
-console.log(Person_Name);
+var Person_Name = "fahad";
+console.log("Lower Case", Person_Name.toLowerCase());
+console.log("Upper Case", Person_Name.toUpperCase());
+console.log("Title Case", Person_Name.replace(/\b\w/g, function (c) { return c.toUpperCase(); }));
--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-let Person_Name : string = "Eric"
+let Person_Name :string ="fahad"
 
-console.log(Person_Name);
+console.log("Lower Case", Person_Name.toLowerCase())
+
+console.log("Upper Case",Person_Name.toUpperCase())
+
+console.log("Title Case",Person_Name.replace(/\b\w/g,c=> c.toUpperCase()))
