ac18a06abfbdd6313701815a39d8077f6cb8f88c
Abdurehman99 <159618780+Abdurehman99@users.noreply.github.com>
2024-05-01 07:09:29 +0500

    24,More conditional statement

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-"use strict";
-let car = "Sabaru";
-console.log("is car == 'Sabaru'? ipredict True");
-console.log(car == Sabaru);
+var car = "Subaru";
+var age = 25;
+var number = [1, 2, 3, 4, 5];
+console.log("is car == 'Subaru?' i prdict true");
+console.log(car == "Subaru"); // True case-sensitive
+console.log("is car == 'Subaru?' i prdict false");
+console.log(car == "subaru"); // False case-sensitive
+console.log("is car != 'Toyota'? i prdict true");
+console.log(car != "Toyota"); // True 
+console.log("is car !== 'subaru'? i prdict false");
+console.log(car !== "subaru"); // False case-sensitive
+console.log("is car.tolowercase() == 'subaru'? i predict true");
+console.log(car.toLowerCase() == 'subaru'); // True convert to lower case
+console.log("is car.tolowercase() === 'subaru'? i predict false");
+console.log(car.toLowerCase()); // False Original value
+console.log("Is age == 25? I predict true");
+console.log(age == 25); // True 
+console.log("Is age != 25? I predict true");
+console.log(age != 25); // True 
+console.log("Is age == 25? I predict true");
+console.log(age == 25); // True 
+console.log("Is age > 30? I predict false");
+console.log(age > 30); // Flase
+console.log("Is age <= 25? I predict true");
+console.log(age <= 25); // True
+console.log("Is age > 30 || age < 18 I predict false");
+console.log(age > 30 || age < 18); // False 
+console.log("Is 3 in numbers? i predict true");
+console.log(3 in number); //True
+console.log("Is 5 not in numbers? i predict false");
+console.log(5 in number); //True
