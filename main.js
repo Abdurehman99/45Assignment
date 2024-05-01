f3add503ce2c7fa9f47e2a641d130cfde5813690
Abdurehman99 <159618780+Abdurehman99@users.noreply.github.com>
2024-05-01 07:21:32 +0500

    37.large T-shirt

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-function make_shirt(size, message) {
-    console.log("Creating a ".concat(size, " shirt with the message: \"").concat(message, "\""));
+"use strict";
+class Shirt {
+    constructor(size = "large", message = "I love TypeScript") {
+        this.size = size;
+        this.message = message;
+    }
 }
-make_shirt('large', 'Hello, world!');
+function make_shirt(size = "medium", message = "I love TypeScript") {
+    return new Shirt(size, message);
+}
+const largeShirt = make_shirt();
+console.log("Large Shirt:", largeShirt);
+const mediumShirt = make_shirt("medium");
+console.log("Medium Shirt:", mediumShirt);
+let customShirt = make_shirt("small", "TypeScript is awesome!");
+console.log("Custom Shirt:", customShirt);
--- main.ts
+++ main.ts
@@ -1,5 +1,24 @@
-function make_shirt(size: string, message: string): void {
-    console.log(`Creating a ${size} shirt with the message: "${message}"`);
+class Shirt {
+    size: string;
+    message: string;
+  
+    constructor(size: string = "large", message: string = "I love TypeScript") {
+        this.size = size;
+        this.message = message;
+    }
   }
   
-  make_shirt('large', 'Hello, world!');
+  function make_shirt(size: string = "medium", message: string = "I love TypeScript") {
+    return new Shirt(size, message);
+  }
+  
+
+  const largeShirt = make_shirt();
+  console.log("Large Shirt:", largeShirt);
+  
+  const mediumShirt = make_shirt("medium");
+  console.log("Medium Shirt:", mediumShirt);
+  
+
+  let customShirt = make_shirt("small", "TypeScript is awesome!");
+  console.log("Custom Shirt:", customShirt);
