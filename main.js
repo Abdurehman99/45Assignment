7cd05db5897116a09c73a7108c086d56e647910d
Abdurehman99 <159618780+Abdurehman99@users.noreply.github.com>
2024-05-01 07:20:02 +0500

    34.Pizzas

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
-var my_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-for (var i = 0; i < my_numbers.length; i++) {
-    if (my_numbers[i] == 1) {
-        console.log("".concat(my_numbers[i], "st"));
-    }
-    if (my_numbers[i] == 2) {
-        console.log("".concat(my_numbers[i], "nd"));
-    }
-    if (my_numbers[i] == 3) {
-        console.log("".concat(my_numbers[i], "rd"));
-    }
-    if (my_numbers[i] >= 4 && my_numbers[i] <= 9) {
-        console.log("".concat(my_numbers[i], "th"));
-    }
+var Favourite_Pizzas = ["MargaritaPizza", "Fajita", "SpicyArabian"];
+for (var i = 0; i < Favourite_Pizzas.length; i++) {
+    console.log(Favourite_Pizzas[i]);
 }
+for (var i = 0; i < Favourite_Pizzas.length; i++) {
+    console.log("I like to eat ".concat(Favourite_Pizzas));
+}
+console.log("I really like to eat pizzas becasue they come in diffrent flavours");
--- main.ts
+++ main.ts
@@ -1,17 +1,10 @@
-let my_numbers : number [] = [1,2,3,4,5,6,7,8,9];
+let Favourite_Pizzas : string [] = ["MargaritaPizza","Fajita","SpicyArabian"]
 
-for ( let i=0; i < my_numbers.length; i++) {
-    if (my_numbers [i] == 1)  {
-        console.log(`${my_numbers[i]}st`)
-     } 
-     if (my_numbers [i] == 2)  {
-        console.log(`${my_numbers[i]}nd`)
-     }
-     if (my_numbers [i] == 3)  {
-        console.log(`${my_numbers[i]}rd`)
-     }
-     if (my_numbers [i] >= 4 && my_numbers [i] <= 9 )  {
-        console.log(`${my_numbers[i]}th`)
-     }
-   
+for ( let i =0 ; i < Favourite_Pizzas.length ; i++  ) {
+
+    console.log(Favourite_Pizzas[i])
 }
+for (let i =0 ; i < Favourite_Pizzas.length ; i++) {
+    console.log(`I like to eat ${ Favourite_Pizzas}`)
+}
+console.log("I really like to eat pizzas becasue they come in diffrent flavours")
