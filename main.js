dd57ba29b342c084b157354c759c95b19b51a50d
Abdurehman99 <159618780+Abdurehman99@users.noreply.github.com>
2024-05-01 07:03:45 +0500

    16.More Guests

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var guest_list = ['aazain', 'hadi', 'rafay', 'rashid'];
-for (var i = 0; i < guest_list.length; i++) {
-    console.log("we invite u on dinner tomorrow ".concat(guest_list[i], "\t\n"));
-}
+// for(let i=0; i<guest_list.length; i++){
+// console.log(`we invite u on dinner tomorrow ${guest_list[i]}\t\n`)
+// }
 var not_attending = "rafay";
 var new_guest = "sulaiman";
 guest_list[2] = new_guest;
@@ -9,3 +9,7 @@ for (var i = 0; i < guest_list.length; i++) {
     console.log("we invite u on dinner tomorrow ".concat(guest_list[i], "\t\n"));
 }
 console.log("".concat(not_attending, " unfortunately cannot attend tomorrow dinner."));
+guest_list.unshift('sana', 'saqib', 'shamin');
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("we invite u on dinner tomorrow because we found a bigger table ".concat(guest_list[i], "\t\n"));
+}
--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 let guest_list : string [] = ['aazain','hadi','rafay','rashid'];
 
-for(let i=0; i<guest_list.length; i++){
+// for(let i=0; i<guest_list.length; i++){
 
-console.log(`we invite u on dinner tomorrow ${guest_list[i]}\t\n`)
-}
+// console.log(`we invite u on dinner tomorrow ${guest_list[i]}\t\n`)
+// }
 
 let not_attending : string = "rafay";
 let new_guest : string = "sulaiman";
@@ -15,3 +15,9 @@ for(let i=0; i<guest_list.length; i++){
     }
 
     console.log(`${not_attending} unfortunately cannot attend tomorrow dinner.`)
+
+    guest_list.unshift('sana','saqib','shamin');
+
+    for(let i=0; i<guest_list.length; i++){
+console.log(`we invite u on dinner tomorrow because we found a bigger table ${guest_list[i]}\t\n`)
+    }
