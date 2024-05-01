474cfb37abb0aba863e6f12786ca54b631de004f
Abdurehman99 <159618780+Abdurehman99@users.noreply.github.com>
2024-05-01 06:54:58 +0500

    12.Greetings

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var names = ['saqib', 'rashid', 'fahad', 'zoaib', 'hadi'];
+var message = "Hope you are fine";
 for (var i = 0; i < names.length; i++) {
-    console.log(names[i]);
+    console.log(names[i], "\t" + message);
 }
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let names : string [] = ['saqib','rashid','fahad','zoaib','hadi'];
-
+let message : string  = "Hope you are fine"
 for(let i=0; i<names.length; i++){
 
-console.log(names[i]);
+console.log(names[i] "\t" + message);
 }
