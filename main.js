315796a35ec914d3a16808fc5282d8793d785860
Abdurehman99 <159618780+Abdurehman99@users.noreply.github.com>
2024-05-01 07:04:36 +0500

    17.Shrinking list

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 var guest_list = ['aazain', 'hadi', 'rafay', 'rashid'];
-// for(let i=0; i<guest_list.length; i++){
-// console.log(`we invite u on dinner tomorrow ${guest_list[i]}\t\n`)
-// }
 var not_attending = "rafay";
 var new_guest = "sulaiman";
 guest_list[2] = new_guest;
 for (var i = 0; i < guest_list.length; i++) {
     console.log("we invite u on dinner tomorrow ".concat(guest_list[i], "\t\n"));
 }
-console.log("".concat(not_attending, " unfortunately cannot attend tomorrow dinner."));
+console.log("".concat(not_attending, " unfortunately cannot attend tomorrow dinner\n."));
 guest_list.unshift('sana', 'saqib', 'shamin');
 for (var i = 0; i < guest_list.length; i++) {
     console.log("we invite u on dinner tomorrow because we found a bigger table ".concat(guest_list[i], "\t\n"));
 }
+console.log("unfortunately we cannot arrange bigger table only two people can come\n");
+while (guest_list.length > 2) {
+    var remove_guest = guest_list.pop();
+    console.log("sorry ".concat(remove_guest, "  you are not invited for dinner\n\t"));
+}
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("sir/ms " + guest_list[i] + "\n youre still invited");
+}
+guest_list.splice(0, 2);
+console.log(guest_list);
--- main.ts
+++ main.ts
@@ -1,10 +1,4 @@
 let guest_list : string [] = ['aazain','hadi','rafay','rashid'];
-
-// for(let i=0; i<guest_list.length; i++){
-
-// console.log(`we invite u on dinner tomorrow ${guest_list[i]}\t\n`)
-// }
-
 let not_attending : string = "rafay";
 let new_guest : string = "sulaiman";
 guest_list[2] = new_guest
@@ -14,10 +8,25 @@ for(let i=0; i<guest_list.length; i++){
     console.log(`we invite u on dinner tomorrow ${guest_list[i]}\t\n`)
     }
 
-    console.log(`${not_attending} unfortunately cannot attend tomorrow dinner.`)
+    console.log(`${not_attending} unfortunately cannot attend tomorrow dinner\n.`)
 
     guest_list.unshift('sana','saqib','shamin');
 
     for(let i=0; i<guest_list.length; i++){
 console.log(`we invite u on dinner tomorrow because we found a bigger table ${guest_list[i]}\t\n`)
     }
+
+    console.log("unfortunately we cannot arrange bigger table only two people can come\n")
+    while(guest_list.length>2){
+        let remove_guest = guest_list.pop();
+        console.log(`sorry ${remove_guest}  you are not invited for dinner\n\t`);
+    }
+    for(let i=0; i<guest_list.length; i++){
+
+console.log(`sir/ms ` + guest_list[i] + "\n youre still invited")
+
+    }
+
+
+    guest_list.splice(0,2)
+    console.log(guest_list)
